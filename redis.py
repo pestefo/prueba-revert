899aadcfc0a3bf7541c84109f53a773b44a5a1fd
Pablo Estefó <pablo.estefo@cornershopapp.com>
2022-02-28 12:46:29 -0300

    Revert "update redis"

--- redis.py
+++ redis.py
@@ -2,7 +2,7 @@ import logging
 
 from sanic import Sanic
 
-import aioredis
+from aioredis import create_redis_pool
 
 logger = logging.getLogger(__name__)
 
@@ -15,8 +15,9 @@ def register_redis(app: Sanic):
 
     @app.listener("before_server_start")
     async def configure_redis(app, loop):
-        logger.debug(f"Creating redis pool on address: {config['url']}")
-        pool = await aioredis.from_url(**config)
+        logger.debug(f"Creating redis pool on address: {config['address']}")
+        pool = await create_redis_pool(loop=loop, **config)
+
         app.redis = pool
         if not hasattr(app, "extensions"):
             app.extensions = {}
@@ -26,20 +27,27 @@ def register_redis(app: Sanic):
     async def close_redis_pool(app, loop):
         if not hasattr(app, "redis"):
             return
+
         pool = app.redis
+
         if pool is not None:
             logger.debug("Closing redis pool")
-            await pool.close()
+            waiter = pool.wait_closed()
+            pool.close()
+            await waiter
 
 
 def get_redis_config(config):
-    protocol = "rediss://" if getattr(config, "REDIS_SSL", True) else "redis://"
-    host = getattr(config, "REDIS_HOST", "localhost")
-    port = str(getattr(config, "REDIS_PORT", 6379))
-    print(protocol + host + ":" + port)
+    connection_uri = (
+        getattr(config, "REDIS_HOST", "localhost"),
+        getattr(config, "REDIS_PORT", 6379),
+    )
     return {
-        "url": protocol + host + ":" + port,
+        "address": connection_uri,
         "db": getattr(config, "REDIS_DATABASE", None),
         "password": getattr(config, "REDIS_PASSWORD", None),
-        "decode_responses": getattr(config, "REDIS_DECODE_RESPONSE", False),
+        "ssl": getattr(config, "REDIS_SSL", None),
+        "encoding": getattr(config, "REDIS_ENCODING", None),
+        "minsize": getattr(config, "REDIS_MIN_SIZE_POOL", 1),
+        "maxsize": getattr(config, "REDIS_MAX_SIZE_POOL", 10),
     }
