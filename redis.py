bc142e2935b46a9bab30aec461bfea14e23f90eb
Pablo Estefó <pablo.estefo@cornershopapp.com>
2022-02-28 12:45:13 -0300

    update redis

--- redis.py
+++ redis.py
@@ -2,7 +2,7 @@ import logging
 
 from sanic import Sanic
 
-from aioredis import create_redis_pool
+import aioredis
 
 logger = logging.getLogger(__name__)
 
@@ -15,9 +15,8 @@ def register_redis(app: Sanic):
 
     @app.listener("before_server_start")
     async def configure_redis(app, loop):
-        logger.debug(f"Creating redis pool on address: {config['address']}")
-        pool = await create_redis_pool(loop=loop, **config)
-
+        logger.debug(f"Creating redis pool on address: {config['url']}")
+        pool = await aioredis.from_url(**config)
         app.redis = pool
         if not hasattr(app, "extensions"):
             app.extensions = {}
@@ -27,27 +26,20 @@ def register_redis(app: Sanic):
     async def close_redis_pool(app, loop):
         if not hasattr(app, "redis"):
             return
-
         pool = app.redis
-
         if pool is not None:
             logger.debug("Closing redis pool")
-            waiter = pool.wait_closed()
-            pool.close()
-            await waiter
+            await pool.close()
 
 
 def get_redis_config(config):
-    connection_uri = (
-        getattr(config, "REDIS_HOST", "localhost"),
-        getattr(config, "REDIS_PORT", 6379),
-    )
+    protocol = "rediss://" if getattr(config, "REDIS_SSL", True) else "redis://"
+    host = getattr(config, "REDIS_HOST", "localhost")
+    port = str(getattr(config, "REDIS_PORT", 6379))
+    print(protocol + host + ":" + port)
     return {
-        "address": connection_uri,
+        "url": protocol + host + ":" + port,
         "db": getattr(config, "REDIS_DATABASE", None),
         "password": getattr(config, "REDIS_PASSWORD", None),
-        "ssl": getattr(config, "REDIS_SSL", None),
-        "encoding": getattr(config, "REDIS_ENCODING", None),
-        "minsize": getattr(config, "REDIS_MIN_SIZE_POOL", 1),
-        "maxsize": getattr(config, "REDIS_MAX_SIZE_POOL", 10),
+        "decode_responses": getattr(config, "REDIS_DECODE_RESPONSE", False),
     }
